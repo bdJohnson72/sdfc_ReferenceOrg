f5de6f64484ccec14b3d9fb99a56039f72fac08c
Brooks Johnson <bjohnson@insulet.com>
2024-01-31 08:04:40 -0500

    jest updates

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,32 @@
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
-
+const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
+setupFilesAfterEnv.push('<rootDir>/jest-sa11y-setup.js');
 module.exports = {
     ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+    moduleNameMapper: {
+        /* CSS library import fix in test context. See:
+        https://github.com/salesforce/sfdx-lwc-jest/issues/288) */
+        '^c/cssLibrary$':
+            '<rootDir>/force-app/main/default/lwc/cssLibrary/cssLibrary.css',
+        // Jest mocks
+        '^@salesforce/apex$': '<rootDir>/force-app/test/jest-mocks/apex',
+        '^@salesforce/schema$': '<rootDir>/force-app/test/jest-mocks/schema',
+        '^lightning/navigation$':
+            '<rootDir>/src/test/jest-mocks/lightning/navigation',
+        '^lightning/platformShowToastEvent$':
+            '<rootDir>/src/test/jest-mocks/lightning/platformShowToastEvent',
+        '^lightning/uiRecordApi$':
+            '<rootDir>/src/test/jest-mocks/lightning/uiRecordApi',
+        '^lightning/messageService$':
+            '<rootDir>/src/test/jest-mocks/lightning/messageService',
+        '^lightning/actions$':
+            '<rootDir>/src/test/jest-mocks/lightning/actions',
+        '^lightning/modal*':
+            '<rootDir>/src/test/jest-mocks/lightning/modal',
+        '^lightning/refresh$':
+            '<rootDir>/src/test/jest-mocks/lightning/refresh'
+    },
+    setupFiles: ['jest-canvas-mock'],
+    setupFilesAfterEnv,
+    testTimeout: 10000
 };
