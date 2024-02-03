b6aa0708dbaf22ec8502d5067dfa89c93467ec67
Brooks Johnson <bjohnson@insulet.com>
2024-02-03 12:40:15 -0500

    BMI Calculator

--- src/main/default/lwc/bmiCalculator/__tests__/bmiCalculator.test.js
+++ src/main/default/lwc/bmiCalculator/__tests__/bmiCalculator.test.js
@@ -24,13 +24,66 @@ describe('c-bmi-calculator test suite', () =>{
         expect(inputs[0].placeholder).toBe('enter height');
         expect(inputs[1].placeholder).toBe('enter weight');
     });
-    it('should render a select field', () => {
+    it('should render a submit button', () => {
         const element = createElement('c-bmi-calculator', {
             is: bmiCalculator
         });
         document.body.appendChild(element);
-        const selectField = element.shadowRoot.querySelector('lightning-combobox');
-        expect(selectField).not.toBe(null);
+        const button = element.shadowRoot.querySelector('button');
+        expect(button.textContent).toBe('Calculate')
+    });
+    it('should calculate bmi', async () => {
+        const element = createElement('c-bmi-calculator', {
+            is: bmiCalculator
+        });
+        document.body.appendChild(element);
+        const button = element.shadowRoot.querySelector('.submit');
+        const height = element.shadowRoot.querySelector('input[name="height"]')
+        const weight = element.shadowRoot.querySelector('input[name="weight"]')
 
+        height.value = 73;
+        weight.value = 210;
+        height.dispatchEvent(new CustomEvent("change"));
+        weight.dispatchEvent(new CustomEvent("change"));
+        button.click();
+        await flushPromises();
+        const form = element.shadowRoot.querySelector('form');
+        const section = element.shadowRoot.querySelector('section');
+        expect(section).not.toBe(null);
+        const bmi = element.shadowRoot.querySelector('.bmi-result');
+        expect(bmi.textContent).toBe('Your BMI is 27.70');
+        console.log(form)
+        expect(form).toBeNull()
+    });
+    it('should handle recalculate logic', async () => {
+        const element = createElement('c-bmi-calculator', {
+            is: bmiCalculator
+        });
+        document.body.appendChild(element);
+        //test initial conditions
+        const results = element.shadowRoot.querySelector('.results');
+        const form = element.shadowRoot.querySelector('form');
+        expect(form).not.toBeNull();
+        expect(results).toBeNull();
+        const height = element.shadowRoot.querySelector('input[name="height"]')
+        const weight = element.shadowRoot.querySelector('input[name="weight"]')
+        const calculateButton = element.shadowRoot.querySelector('.submit')
+        height.value = 73;
+        weight.value = 210;
+        height.dispatchEvent(new CustomEvent('change'));
+        weight.dispatchEvent(new CustomEvent('change'));
+        calculateButton.click();
+        await flushPromises();
+        const newForm = element.shadowRoot.querySelector('form');
+        const newResults = element.shadowRoot.querySelector('.results');
+        expect(newForm).toBeNull();
+        expect(newResults).not.toBe(null);
+        const recalcButton = element.shadowRoot.querySelector('.button-recalc');
+        recalcButton.click();
+        await flushPromises();
+        const postRecalcResults = element.shadowRoot.querySelector('.results');
+        const postRecalcForm = element.shadowRoot.querySelector('form');
+        expect(postRecalcForm).not.toBeNull();
+        expect(postRecalcResults).toBeNull();
     });
 })
--- src/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ src/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -3,24 +3,46 @@
  */
 
 import {LightningElement} from 'lwc';
+import {loadStyle} from "lightning/platformResourceLoader";
+import styles from '@salesforce/resourceUrl/globalCSS'
 
 export default class BmiCalculator extends LightningElement {
 
-    system='US';
-
-    get options(){
-        return [
-            {label: 'US', value: 'US'},
-            {label: 'Metric', value: 'Metric'}
-        ]
+    height = "";
+    weight = "";
+    bmi = "";
+    result = "";
+    async connectedCallback() {
+        try{
+            await loadStyle(this, styles)
+        }catch (e){
+            console.error(e)
+        }
     }
     handleInputChange(event){
-
+       const {name, value} = event.target;
+       name === 'height' ? this.height = value : this.weight = value;
     }
-
-    handleSystemChange(event){
-        this.system = event.detail.value;
-        console.log(this.system)
+    submitHandler(event){
+        event.preventDefault();
+        this.calculateBMI();
     }
-
+    calculateBMI(){
+        let tempBMI = (Number(this.weight) * 703) / (Number(this.height) * (this.height));
+        tempBMI.toFixed(2);
+        this.bmi = tempBMI.toFixed(2);
+        console.log(this.bmi)
+        if (this.bmi < 18.5){
+            this.result = 'Underweight';
+        }else if(this.bmi < 25 ){
+            this.result = 'healthy';
+        }else if (this.bmi > 25 && this.bmi < 30 ){
+            this.result = 'overweight';
+        }else {
+            this.result = 'Obese'
+        }
+    }
+    handleRecalculate(){
+        this.result = '';
+    }
 }
